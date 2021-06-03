3199cd35883d299fd1c848feb5627e993c37c223
Jered <JeredL@gmail.com>
2021-06-02 19:24:25 -0500

    base html page setup

--- www/app.js
+++ www/app.js
@@ -2,17 +2,22 @@
 // JDLinares
 // 2 June 2021
 
+var basehtml = require('./view/baseView')
+
+
+
 const http = require('http')
 const port = 3000 	//process.env.PORT				// NOTE: Default 3000
 
 const server = http.createServer((req,res) => {
 		res.statusCode = 200
 		res.setHeader('Content-Type','text/html')
-		res.end('<h1>Hello World</h1>')
+		res.end(basehtml.base)
 })
 
 server.listen(port,()=>{
 		console.log(`Server running at port ${port}`)
+		console.log(basehtml)
 })
 
 
